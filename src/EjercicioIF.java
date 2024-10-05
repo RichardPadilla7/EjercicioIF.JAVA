9ecfbd5b7be1360a383c722ce29f85c9b490e71a
Richard Padilla <150530717+RichardPadilla7@users.noreply.github.com>
2024-10-05 11:20:25 -0500

    richard

--- src/EjercicioIF.java
+++ src/EjercicioIF.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+//Richard Padilla
+import java.util.Scanner;
 
 public class EjercicioIF {
+    public static void main(String[] args) {
+
+        int edad;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese su edad: ");
+        edad = input.nextInt();
+
+        if (edad >= 18){
+            System.out.println("Usted es mayor de edad :)");
+        }  else if (edad < 18){
+            System.out.println("Usted es menor de edad :(");
+        }
+    }
 }
